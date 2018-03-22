d81defe0351948a96ece5375d45e7a3cdcd4ba16
josue <jcatota@digitalstrategy.com.ec>
2018-03-22 10:34:41 -0500

    creado controlador api cliente, añadido utils->response

---
+++ VentaServicios/VentaServicios/Controllers/API/ClientesController.cs
@@ -0,0 +1,76 @@
+﻿using bd.swth.entidades.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using VentaServicios.ModeloDato;
+
+namespace VentaServicios.Controllers.API
+{
+
+
+    [RoutePrefix("api/Clientes")]
+    public class ClientesController : ApiController
+    {
+        private readonly ModelVentas db = new ModelVentas();
+
+
+        // GET: api/Clientes
+        [HttpGet]
+        [Route("ListarClientes")]
+        public async Task<List<Cliente>> GetCliente()
+        {
+            try
+            {
+                return await db.Cliente.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                return new List<Cliente>();
+            }
+        }
+
+
+
+
+        /*
+
+        // POST: api/Clientes
+        [HttpPost]
+        [Route("InsertarCliente")]
+        public async Task<Response> InsertarCliente([FromBody] Cliente cliente)
+        {
+
+            Response response = new Response();
+
+            if (!ModelState.IsValid)
+            {
+
+            }
+
+            try
+            {
+                db.Cliente.Add(cliente);
+                await db.SaveChangesAsync();
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+
+        }
+
+        return response;
+
+    }
+    */
+    }
+}
---
+++ VentaServicios/VentaServicios/Utils/Response.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace bd.swth.entidades.Utils
+{
+    public  class Response
+    {
+        public  bool IsSuccess { get; set; }
+        public  string Message { get; set; }
+        public object Resultado { get; set; }
+
+    }
+}
