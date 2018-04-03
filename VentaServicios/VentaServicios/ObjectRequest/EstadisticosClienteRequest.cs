9d1239e7a5b27e0e17252f2d240bee068d23c54b
Néstor Valle <nestorvalle880416@gmail.com>
2018-04-02 19:46:28 -0500

    Funcionalidad gráfico de cliente

---
+++ VentaServicios/VentaServicios/ObjectRequest/EstadisticosClienteRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VentaServicios.ObjectRequest
+{
+    public class EstadisticosClienteRequest
+    {
+        // Campos de la tabla vendedor
+        public int IdCliente { get; set; }
+
+        public int? CompromisosCumplidos { get; set; }
+
+        public int? CompromisosIncumplidos { get; set; }
+
+        public List<TipoCompromisoRequest> ListaTipoCompromiso { get; set; }
+    }
+}
